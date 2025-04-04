38753260be13637c2ffc80c5a0aa4abd8fc72090
n3tw0rth <rtheekshanafernando@gmail.com>
2025-04-04 19:49:00 +0530

    chore: changes

--- next.config.mjs
+++ next.config.mjs
@@ -8,14 +8,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
-  },
 }
 
 if (userConfig) {
